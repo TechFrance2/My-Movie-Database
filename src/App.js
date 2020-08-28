b223a6a68afe3cdb77e0e95c61920a5630b5f278
Franci Nakamura <franci.nakamura100@gmail.com>
2020-08-28 01:28:01 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SearchMovies from './SearchMovies';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+       <h1 className="title"> 
+        My Movie Search
+       </h1>
+       <SearchMovies/>
+
+         
+       
+      
+    </div>
+    
+  );
+}
+
+export default App;
